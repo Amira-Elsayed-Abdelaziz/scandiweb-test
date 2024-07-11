84b68076f1dd848f95f415d4abb58b3b1f93984d
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-07-11 10:29:39 +0300

    Test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <>
-      <nav className="sticky-top navbar bg-info-subtle border-bottom border-body navbar-expand-lg " data-bs-theme="dark">
+      <nav className="navbar bg-info-subtle border-bottom border-body navbar-expand-lg " data-bs-theme="dark">
         <div className="container-fluid navbar-brand col-7">
           Scandiweb-Test
         </div>
